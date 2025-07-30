04ccbdd569f82e20c3ef2696c55e3fb5c24eb967
jn-njjp <114713582+jn-njjp@users.noreply.github.com>
2025-07-30 14:07:15 +0900

    - Added several new features to the main page. - Updated dictionary.

--- docs/mto.js
+++ docs/mto.js
@@ -25,20 +25,90 @@ function deleteIVS(str) {
     return dec_str;
 }
 
-function replaceStrings2(jisyo1, jisyo2, flag) {
+function deleteIVS2(){
+    var str = document.mto.bef.value;
+    processedStr = deleteIVS(str);
+    document.mto.aft.value = processedStr;
+}
+
+function executeBasedOnCheckbox2(jisyo1, jisyo2, jisyo3, flag) {
+    var small_kana = document.forms.mto.small_kana_yes.checked;
+    if (small_kana === true) {//捨假名がオンのとき
+        if (flag === 0) {//新から舊
+            return replaceStrings3(jisyo1, jisyo2, jisyo3, flag);
+        } else if (flag === 1) {//舊から新
+            return replaceStrings3(jisyo1, jisyo2, jisyo3, flag);
+        }
+    } else {//捨假名がオフのとき
+        if (flag === 0) {//新から舊
+            return replaceStrings2(jisyo2, jisyo3, flag);
+        } else if (flag === 1) {//舊から新
+            return replaceStrings2(jisyo1, jisyo2, flag);
+        }
+    }
+}
+
+function executeBasedOnCheckbox(jisyo1, jisyo2, flag) {
+    var small_kana = document.forms.mto.small_kana_yes.checked;
+    if (small_kana === true) {//捨假名がオンのとき
+        if (flag === 0) {//新から舊
+            return replaceStrings2(jisyo1, jisyo2, flag);
+        } else if (flag === 1) {//舊から新
+            return replaceStrings2(jisyo1, jisyo2, flag);
+        }
+    } else {//捨假名がオフのとき
+        if (flag === 0) {//新から舊
+            return replaceStrings(jisyo2, flag);
+        } else if (flag === 1) {//舊から新
+            return replaceStrings(jisyo1, flag);
+        }
+    }
+}
+
+function replaceStrings3(jisyo1, jisyo2, jisyo3, flag) {
     var str = document.mto.bef.value;
     var del_ivs = document.forms.mto.del_IVS_yes.checked
     if (del_ivs == true) {
         str = deleteIVS(str);
     }
-    if (flag == 0) {
+    if (flag == 0) {//新から舊
         for (var i = 0; i < jisyo1.length; i++) {
             str = gsub(str, jisyo1[i][0], jisyo1[i][1]);
         }
         for (var i = 0; i < jisyo2.length; i++) {
             str = gsub(str, jisyo2[i][0], jisyo2[i][1]);
         }
-    } else if (flag == 1) {
+        for (var i = 0; i < jisyo3.length; i++) {
+            str = gsub(str, jisyo3[i][0], jisyo3[i][1]);
+        }
+    } else if (flag == 1) {//舊から新
+        for (var i = 0; i < jisyo1.length; i++) {
+            str = gsub(str, jisyo1[i][1], jisyo1[i][0]);
+        }
+        for (var i = 0; i < jisyo2.length; i++) {
+            str = gsub(str, jisyo2[i][1], jisyo2[i][0]);
+        }
+        for (var i = 0; i < jisyo3.length; i++) {
+            str = gsub(str, jisyo3[i][1], jisyo3[i][0]);
+        }
+    } 
+    document.mto.aft.value = str;
+}
+
+function replaceStrings2(jisyo1, jisyo2, flag) {
+    var str = document.mto.bef.value;
+    var del_ivs = document.forms.mto.del_IVS_yes.checked
+    if (del_ivs == true) {
+        str = deleteIVS(str);
+    }
+    if (flag == 0) {//新から舊
+        for (var i = 0; i < jisyo1.length; i++) {
+            str = gsub(str, jisyo1[i][0], jisyo1[i][1]);
+        }
+        for (var i = 0; i < jisyo2.length; i++) {
+            str = gsub(str, jisyo2[i][0], jisyo2[i][1]);
+        }
+    } else if (flag == 1) {//舊から新
         for (var i = 0; i < jisyo1.length; i++) {
             str = gsub(str, jisyo1[i][1], jisyo1[i][0]);
         }
@@ -55,11 +125,11 @@ function replaceStrings(jisyo, flag) {
     if (del_ivs == true) {
         str = deleteIVS(str);
     }
-    if (flag == 0) {
+    if (flag == 0) {//新から舊
         for (var i = 0; i < jisyo.length; i++) {
             str = gsub(str, jisyo[i][0], jisyo[i][1]);
         }
-    } else if (flag == 1) {
+    } else if (flag == 1) {//舊から新
         for (var i = 0; i < jisyo.length; i++) {
             str = gsub(str, jisyo[i][1], jisyo[i][0]);
         }
@@ -68,10 +138,12 @@ function replaceStrings(jisyo, flag) {
 }
 
 function dictElements() {
-    var total = kanaArray.length + kanjiArray.length;
+    var total = kanaArray.length + smallArray.length + kanjiArray.length + jionArray.length;
+    var kana_length = kanaArray.length + smallArray.length;
     document.write("<p>現時点での辞書の要素数は " + total +
-                   "（かな変換用:" + kanaArray.length +
-                   " 漢字変換用:" + kanjiArray.length + "）</p>");
+                   "（仮名変換用:" + kana_length + 
+                   " 漢字変換用:" + kanjiArray.length + 
+                   " 読み仮名変換用:" + jionArray.length + "）</p>");//要素数表示に読み仮名も追加
 }
 
 function readFileInLocal() {
